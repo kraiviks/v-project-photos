405ed5a74bf3a25f6047e92ca8731bff55195769
kraiviks <kraiviks@gmail.com>
2024-10-29 20:34:44 +0200

    feat: add notfound page

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,11 +8,18 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
+import NotFound from '@/components/NotFound.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: setupLayouts(routes).concat([
+		{
+			path: '/:catchAll(.*)',
+			component: NotFound,
+		},
+	]),
+});
+
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
