1837165fab7b7e796903392dd0ba35f424664877
kraiviks <kraiviks@gmail.com>
2024-10-29 19:17:54 +0200

    remove unused hook

--- src/hooks/useRoute.ts
+++ src/hooks/useRoute.ts
@@ -5,18 +5,8 @@ type RouteWithId = RouteLocationNormalizedLoaded & {
   params: { id: string };
 };
 
-type RouteWithQuery = RouteLocationNormalizedLoaded & {
-  query: { [key: string]: string };
-};
-
 export function useRouteId() {
   const route = useRoute();
   const id = (route.params as RouteWithId["params"]).id;
   return id;
-}
-
-export function useRouteQuery<T extends string>(key: T) {
-  const route = useRoute();
-  const query = (route.query as RouteWithQuery["query"])[key];
-  return query;
 }
