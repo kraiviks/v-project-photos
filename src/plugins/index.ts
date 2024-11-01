097bff8a5dca98fe42f2ba1c285283cce73f0dfc
kraiviks <kraiviks@gmail.com>
2024-11-01 11:42:53 +0200

    feat: added pinia-plugin-persistedstate

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -8,11 +8,13 @@
 import vuetify from "./vuetify";
 import { createPinia } from "pinia";
 import router from "../router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 // Types
 import type { App } from "vue";
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 export function registerPlugins(app: App) {
   app.use(vuetify).use(router).use(pinia);
