8412453e2f9f836ed3406a916a415417e4573f35
kraiviks <kraiviks@gmail.com>
2024-11-03 23:44:17 +0200

    update: ChangeAvatarModal

--- src/components/modals/ChangeAvatarModal.vue
+++ src/components/modals/ChangeAvatarModal.vue
@@ -16,7 +16,7 @@
         text="Please choose an avatar"
         title="Change avatar"
       >
-        <v-text-field v-model="loadedAvatar"> </v-text-field>
+        <v-text-field v-model="loadedAvatar" label="Avatar URL"></v-text-field>
         <v-avatar
           color="surface-variant"
           :image="avatar"
@@ -24,21 +24,25 @@
           class="ma-auto"
         />
         <template v-slot:actions>
-          <v-btn text="Default" @click.stop="selectDefaulrAvatar"></v-btn>
           <v-btn
-            color="surface-variant"
-            text="Updated"
-            @click.stop="updatedAvatar"
-          ></v-btn>
-          <v-btn
-            color="btn-primary"
-            text="Save"
+            text="Default"
             @click.stop="
               () => {
+                selectDefaultAvatar();
                 isActive.value = false;
               }
             "
-          ></v-btn>
+          >Default</v-btn>
+          <v-btn
+            color="surface-variant"
+            text="Updated"
+            @click.stop="
+              () => {
+                updateAvatar();
+                isActive.value = false;
+              }
+            "
+          >Updated</v-btn>
         </template>
       </v-card>
     </template>
@@ -46,17 +50,46 @@
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import { useStorage } from "@vueuse/core";
 import defaultAvatar from "@/assets/avatar.png";
 
 const loadedAvatar = useStorage("avatar", "");
 const avatar = ref(loadedAvatar.value || defaultAvatar);
 
-const selectDefaulrAvatar = () => {
-  avatar.value = defaultAvatar;
+// Function to check if an image is available
+const checkImageAvailability = (url: string, callback: (isAvailable: boolean) => void) => {
+  const img = new Image();
+  img.src = url;
+
+  img.onload = () => callback(true); // If the image is available
+  img.onerror = () => callback(false); // If the image is not available
 };
 
-const updatedAvatar = () => {
-  avatar.value = loadedAvatar.value;
+// Watches for changes in loadedAvatar and updates avatar with availability check
+watch(loadedAvatar, (newVal) => {
+  if (newVal) {
+    checkImageAvailability(newVal, (isAvailable) => {
+      avatar.value = isAvailable ? newVal : defaultAvatar;
+    });
+  } else {
+    avatar.value = defaultAvatar;
+  }
+});
+
+const selectDefaultAvatar = () => {
+  avatar.value = defaultAvatar;
+  loadedAvatar.value = "";
+};
+
+const updateAvatar = () => {
+  if (loadedAvatar.value) {
+    checkImageAvailability(loadedAvatar.value, (isAvailable) => {
+      avatar.value = isAvailable ? loadedAvatar.value : defaultAvatar;
+    });
+  } else {
+    avatar.value = defaultAvatar;
+  }
 };
 </script>
+
